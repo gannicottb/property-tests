2118f4b1023e58c47153f8360bc3eaf3197026a8
Brandon Gannicott <brandon.gannicott@ziffmedia.com>
2020-05-04 18:30:12 -0500

    remove addition and subtraction, add map and cross

--- src/test/scala/DemoSpec.scala
+++ src/test/scala/DemoSpec.scala
@@ -1,30 +1,48 @@
+import zio.random.Random
 import zio.test._
 import zio.test.Assertion._
 
 object DemoSpec extends DefaultRunnableSpec {
   def spec =
     suite("demo")(
-      testM("test add") {
-        def add(a: Int, b: Int): Int = a + b
-
-        val anyPositiveInt = Gen.int(0, Int.MaxValue / 2) // avoid overflow wrapping (maxValue + maxValue = -2)
-        check(anyPositiveInt.zip(anyPositiveInt)) {
-          case (a, b) => {
-            val result = add(a, b)
-            assert(result)(isGreaterThan(a) && isGreaterThan(b))
-          }
-        }
-      },
       testM("test string concat") {
         def concat(a: String, b: String, c: String) = s"$a$b$c"
 
-        val anyThreeStrings = Gen.vectorOfN(3)(Gen.anyString)
-        check(anyThreeStrings) {
-          case Vector(a, b, c) =>
+        check(Gen.anyString, Gen.anyString, Gen.anyString) {
+          case (a, b, c) =>
             assert(concat(a, b, c))(
               containsString(a) && containsString(b) && containsString(c)
             )
         }
+      },
+      testM("map") {
+        def barChecker(s: String): Boolean = s.startsWith("bar-")
+        val specialStrings = Gen.anyString.map(s => s"bar-$s")
+        check(specialStrings) { s =>
+          assert(barChecker(s))(isTrue)
+        }
+      },
+      testM("cross") {
+        // Turns ("foo", 0) into "fff"
+        def replacer(s: String, index: Int): String = {
+          Vector.fill(s.length)(s.charAt(index)).mkString
+        }
+
+        val stringAndIndex = Gen.anyString
+          .cross(Gen.anyInt)
+          .map {
+            case (s, i) if s.nonEmpty => (s, i.abs % s.length)
+            case _                    => ("a", 0) // we aren't testing with empty strings here
+          }
+
+        check(stringAndIndex) {
+          case (s, i) =>
+            val result = replacer(s, i)
+            assert(result.length)(equalTo(s.length)) && assert(result)(
+              equalTo(s.charAt(i).toString * s.length)
+            )
+        }
+
       }
     )
 }
